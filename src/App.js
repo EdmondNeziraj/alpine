022ab462fefa6b9d0f925079ca6ac3b2c5907e6e
Edmond Neziraj <edmondneziraj10@gmail.com>
2023-08-18 18:42:23 -0400

    worked on some styling for the app file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import { styled } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
 
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`
+
 function App() {
   return (
-    <div>
+    <>
       <GlobalStyles />
-      <H1>Alpine</H1>
-    </div>
+      <StyledApp>
+        <H1>Alpine</H1>
+        <Button>test</Button>
+        <Input></Input>
+      </StyledApp>
+    </>
   );
 }
 

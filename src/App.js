688471f30cc42c0ea3b4f6cbecc4a44740dfee72
Edmond Neziraj <edmondneziraj10@gmail.com>
2023-08-18 18:53:10 -0400

    implemented some reusable components for headings

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import { styled } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
 
 const StyledApp = styled.main`
   background-color: orangered;
@@ -18,9 +15,17 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Alpine</H1>
+        <Heading as="h1">Alpine</Heading>
         <Button>test</Button>
-        <Input></Input>
+        <Input type="Number" placeholder="Number of guests"/>
+
+        <Heading as="h2">Check in</Heading>
+        <Button>test</Button>
+        <Input type="Number" placeholder="Number of guests"/>
+
+        <Heading as="h3">Form</Heading>
+        <Button>test</Button>
+        <Input type="Number" placeholder="Number of guests"/>
       </StyledApp>
     </>
   );

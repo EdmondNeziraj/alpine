92d3a4bb2a43dfdecb2ab598aa56cb8b63e9a7a1
Edmond Neziraj <edmondneziraj10@gmail.com>
2023-08-22 21:02:00 -0400

    added react query to the project

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://eeclgnluzryxgynrmokv.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlY2xnbmx1enJ5eGd5bnJtb2t2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI2NzAzNTIsImV4cCI6MjAwODI0NjM1Mn0.plnHd6zulH-0m9Q5e1XMoJNJAGKsIUrRPbGNuG2EzbU";
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;

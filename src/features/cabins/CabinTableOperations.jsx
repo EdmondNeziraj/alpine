eb7ef6881966093ed7c90e43d1bd369544cac80b
Edmond Neziraj <edmondneziraj10@gmail.com>
2023-08-30 14:46:22 -0400

    implemented the sorting feature

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -11,7 +11,7 @@ function CabinTableOperations() {
                 { value: "with-discount", label: "With discount" }
             ]} />
 
-            <SortBy oprtion={[
+            <SortBy options={[
                 { value: "name-asc", label: "Sort by name (A-Z)" },
                 { value: "name-desc", label: "Sort by name (Z-A)" },
                 { value: "regularPrice-asc", label: "Sort by price (low first)" },
--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,8 +1,22 @@
-function SortBy() {
+import { useSearchParams } from "react-router-dom";
+import Select from "./Select";
+
+function SortBy({ options }) {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const sortBy = searchParams.get("sortBy") || "";
+
+    function handleChange(e) {
+        searchParams.set('sortBy', e.target.value);
+        setSearchParams(searchParams);
+    }
+
     return (
-        <div>
-            Sort
-        </div>
+        <Select
+            options={options}
+            type="white"
+            value={sortBy}
+            onChange={handleChange}
+        />
     )
 }
 

55afa9149ee42e89c92f5ffe01689cb61aec0fea
Edmond Neziraj <edmondneziraj10@gmail.com>
2023-10-17 20:23:19 -0400

    implemented the protected route component

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+function ProtectedRoute({ children }) {
+    // load authenticated user
+    
+    // while loading, show spinner
+
+    // if no authenticated user, redirect to the /login
+
+    // if there is user, render the app
+
+    return (
+        { children }
+    );
+}
+
+export default ProtectedRoute;

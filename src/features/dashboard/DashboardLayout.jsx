b910f52466d764c4c3313a3bdc9c0be500c5ad44
Edmond Neziraj <edmondneziraj10@gmail.com>
2023-10-24 15:39:18 -0400

    implemented the display of pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -28,6 +29,8 @@ function DashboardLayout() {
         numDays={numDays}
         cabins={cabins}
       />
+      <div>today</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   )
